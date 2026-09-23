18a14676697446f2baa92089eea0883dd76fe035
Mykhailo Bankov <mishaban8@gmail.com>
2026-09-23 13:41:50 +0100

    Add research references for Week 1 exercise

--- week01/employee-app/src/main/kotlin/Main.kt
+++ week01/employee-app/src/main/kotlin/Main.kt
@@ -18,6 +18,9 @@ fun main() {
     printPaySlip()
 }
 
+// I used these links to check how to format money to 2 decimal places:
+// https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.text/format.html
+// https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/Formatter.html
 
 fun printPaySlip() {
 
